d96e4d0497cc04115a8385009eeb47cec9d0376d
ArtemValt <artemvalt12@gmail.com>
2022-07-06 16:16:34 +0400

    add logger and pac security

--- src/main/java/com/opencode/practice/service/impl/UserServiceImpl.java
+++ src/main/java/com/opencode/practice/service/impl/UserServiceImpl.java
@@ -1,44 +1,33 @@
 package com.opencode.practice.service.impl;
 
-import com.opencode.practice.model.Question;
 import com.opencode.practice.model.Questionnaire;
-import com.opencode.practice.repos.AnswerRepo;
 import com.opencode.practice.repos.QuestionnaireRepo;
 import com.opencode.practice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Service
 
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private QuestionnaireRepo questionnaireRepo;
-    @Autowired
-    private AnswerRepo answerRepo;
+
 
     @Override
     public List<Questionnaire> findAllQuestionnaire() {
         return questionnaireRepo.findAll();
     }
 
-    ////Must make
     @Override
-    public void saveAnswers(List<Integer> answers, long questionaireId, long userId) {
-        Questionnaire questionnaire = questionnaireRepo.findById(questionaireId).get();
-        List<Question> questions = questionnaire.getQuestions();
-        HashMap<Long, Integer> questionsAndAnswers = new HashMap<>();
+    public void saveAnswers(List answers, long id) {
 
-        for(int i = 0; i < answers.size(); i++) {
-            questionsAndAnswers.put(questions.get(i).getId(), answers.get(i));
-        }
     }
 
     @Override
     public Questionnaire getQuestionnaireById(long id) {
         return questionnaireRepo.findById(id).get();
     }
-
 }

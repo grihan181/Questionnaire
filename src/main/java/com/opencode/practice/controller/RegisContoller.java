0b522990e5149869807c3b7170ab750e63f38bbe
ArtemValt <artemvalt12@gmail.com>
2022-07-08 15:24:49 +0400

    add spring-security(jwt token)

---
+++ src/main/java/com/opencode/practice/controller/RegisContoller.java
@@ -0,0 +1,31 @@
+package com.opencode.practice.controller;
+
+import com.opencode.practice.model.Role;
+import com.opencode.practice.model.Status;
+import com.opencode.practice.model.User;
+import com.opencode.practice.repos.UserRepositorySecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/regis")
+public class RegisContoller {
+
+    @Autowired
+    private UserRepositorySecurity userRepositorySecurity;
+
+
+    @PostMapping
+    public void create(@RequestBody User user) {
+        user.setPassword(String.valueOf(new BCryptPasswordEncoder(12).encode(user.getPassword())));
+        user.setRole(Role.USER);
+        user.setStatus(Status.ACTIVE);
+        userRepositorySecurity.save(user);
+    }
+
+
+}

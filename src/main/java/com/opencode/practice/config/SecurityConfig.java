6f4c25827073adc256c429d8ea7296aad8871afb
ArtemValt <artemvalt12@gmail.com>
2022-07-08 15:24:49 +0400

    add spring-security(jwt token)

--- src/main/java/com/opencode/practice/config/SecurityConfig.java
+++ src/main/java/com/opencode/practice/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/v1/auth/signIn").permitAll()
+                .antMatchers("/api/regis").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()

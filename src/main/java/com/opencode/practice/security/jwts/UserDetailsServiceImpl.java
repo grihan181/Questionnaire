f548462af25c8df427c08d8441ea79d6e852fe24
ArtemValt <artemvalt12@gmail.com>
2022-07-08 11:59:37 +0400

    add spring-security(jwt token)

---
+++ src/main/java/com/opencode/practice/security/jwts/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.opencode.practice.security.jwts;
+
+import com.opencode.practice.model.User;
+import com.opencode.practice.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User doesn't exists"));
+        return SecurityUser.fromUser(user);
+    }
+}

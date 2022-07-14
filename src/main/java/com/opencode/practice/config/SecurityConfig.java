733e5a180595f840b590f149c72b862b6ce13ccc
grihan181 <52749469+grihan181@users.noreply.github.com>
2022-07-14 23:23:13 +0400

    ex SecurityConfig

--- src/main/java/com/opencode/practice/config/SecurityConfig.java
+++ src/main/java/com/opencode/practice/config/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * @author Artem
